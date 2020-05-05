0c7ab9d9879b69c5937b0ae64318b0ca600bfc83
Isandre47 <nuanda@live.fr>
2020-05-05 23:50:39 +0200

    add site index

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -2,18 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\SiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class SiteController
+ * @package App\Controller
+ * @Route("/site")
+ */
 class SiteController extends AbstractController
 {
     /**
-     * @Route("/site", name="site")
+     * @Route("/", name="site_index")
      */
-    public function index()
+    public function index(SiteRepository $siteRepository): Response
     {
-        return $this->render('site/index.html.twig', [
+        return $this->render('admin/site/index.html.twig', [
             'controller_name' => 'SiteController',
+            'sites' => $siteRepository->findAll(),
         ]);
     }
 }
