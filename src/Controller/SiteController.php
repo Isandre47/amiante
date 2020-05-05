9fb1e98798e916f6826c7dfb43e9ce9177ba62b4
Isandre47 <nuanda@live.fr>
2020-05-06 00:05:20 +0200

    add edit site

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Site;
+use App\Form\SiteType;
 use App\Repository\SiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,6 +25,25 @@ class SiteController extends AbstractController
         return $this->render('admin/site/index.html.twig', [
             'controller_name' => 'SiteController',
             'sites' => $siteRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="site_edit")
+     */
+    public function edit(Request $request, Site $site): Response
+    {
+        $formSite = $this->createForm(SiteType::class, $site);
+        $formSite->handleRequest($request);
+
+        if ($formSite->isSubmitted() && $formSite->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('site_index');
+        }
+
+        return $this->render('admin/site/edit.html.twig',[
+            'formSite' => $formSite->createView(),
         ]);
     }
 }
