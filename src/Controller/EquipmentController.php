634a14d37e95441cd06bfe9e39806a42804a6c4e
Isandre47 <nuanda@live.fr>
2020-05-06 00:13:22 +0200

    add edit Equipment

--- src/Controller/EquipmentController.php
+++ src/Controller/EquipmentController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Equipment;
+use App\Form\EquipmentType;
 use App\Repository\EquipmentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -21,6 +25,25 @@ class EquipmentController extends AbstractController
         return $this->render('admin/equipment/index.html.twig', [
             'controller_name' => 'EquipmentController',
             'equipments' => $equipmentRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="equipment_edit")
+     */
+    public function edit(Request $request, Equipment $equipment): Response
+    {
+        $formEquipment = $this->createForm(EquipmentType::class, $equipment);
+        $formEquipment->handleRequest($request);
+
+        if ($formEquipment->isSubmitted() && $formEquipment->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('equipment_index');
+        }
+
+        return $this->render('admin/equipment/edit.html.twig',[
+            'formEquipment' => $formEquipment->createView(),
         ]);
     }
 }
