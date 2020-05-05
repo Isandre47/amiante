888172036e34a94ed8a927996efe1b91a5815ec3
Isandre47 <nuanda@live.fr>
2020-05-06 00:10:05 +0200

    add edit Category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
@@ -11,11 +15,30 @@ class CategoryController extends AbstractController
     /**
      * @Route("/category", name="category_index")
      */
-    public function index(CategoryRepository $categoryRepository)
+    public function index(CategoryRepository $categoryRepository): Response
     {
         return $this->render('admin/category/index.html.twig', [
             'controller_name' => 'CategoryController',
             'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/category/edit/{id}", name="category_edit")
+     */
+    public function edit(Request $request, Category $category): Response
+    {
+        $formCategory = $this->createForm(CategoryType::class, $category);
+        $formCategory->handleRequest($request);
+
+        if ($formCategory->isSubmitted() && $formCategory->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('site_index');
+        }
+
+        return $this->render('admin/category/edit.html.twig',[
+            'formCategory' => $formCategory->createView(),
         ]);
     }
 }
