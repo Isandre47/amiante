74f8414f696c1e616ff811695bbc2a0626a573d9
Isandre47 <nuanda@live.fr>
2020-05-05 23:57:41 +0200

    add category index

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/category", name="category")
+     * @Route("/category", name="category_index")
      */
-    public function index()
+    public function index(CategoryRepository $categoryRepository)
     {
-        return $this->render('category/index.html.twig', [
+        return $this->render('admin/category/index.html.twig', [
             'controller_name' => 'CategoryController',
+            'categories' => $categoryRepository->findAll(),
         ]);
     }
 }
