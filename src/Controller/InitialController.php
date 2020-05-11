db6caf9da02c043caa8dd73b638e074f1069e65b
Isandre47 <nuanda@live.fr>
2020-05-12 00:44:56 +0200

    alpha initial analysis

--- src/Controller/InitialController.php
+++ src/Controller/InitialController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Initial;
+use App\Entity\Site;
+use App\Entity\Zone;
 use App\Form\InitialType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -31,10 +34,30 @@ class InitialController extends AbstractController
     public function new(Request $request)
     {
         $initial =  new Initial();
-        $formInitial = $this->createForm(InitialType::class);
+        $formInitial = $this->createForm(InitialType::class, $initial);
+        $formInitial->handleRequest($request);
 
-        return $this->render('admin/initial/edit.html.twig', [
+        if ($formInitial->isSubmitted() && $formInitial->isValid()) {
+            dd($request, $initial);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($initial);
+            $em->flush();
+
+            return $this->redirectToRoute('equipment_index');
+        }
+
+        return $this->render('admin/initial/new.html.twig', [
             'formInitial' => $formInitial->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/zonebysiteid/{id}", name="zonebysiteid")
+     */
+    public function zoneBySiteId(Site $site)
+    {
+        $listphasebysite = $this->getDoctrine()->getRepository(Zone::class)->findBy(['site'=> $site->getId()]);
+
+        return $this->render('admin/initial/select.html.twig', ['list' => $listphasebysite]);
     }
 }
--- src/Form/InitialType.php
+++ src/Form/InitialType.php
@@ -3,35 +3,39 @@
 namespace App\Form;
 
 use App\Entity\Initial;
+use App\Entity\Site;
 use App\Entity\Zone;
 use App\Repository\ZoneRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class InitialType extends AbstractType
 {
-    private $zoneRepository;
-
-    /**
-     * InitialType constructor.
-     * @param $zoneRepository
-     */
-    public function __construct(ZoneRepository $zoneRepository)
-    {
-        $this->zoneRepository = $zoneRepository;
-    }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('location')
-            ->add('zone', EntityType::class, [
-                'class' => Zone::class,
-                'choices' => $this->zoneRepository->findBy(['site' => '1']),
-                'choice_label' => 'category.name'
+            ->add('location', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
             ])
+            ->add('zone', EntityType::class, [
+                'class' => Site::class,
+                'choice_label' => 'name',
+                'placeholder' => 'Choisissez une zone',
+                // A voir si avec un form dédié pour la zone et son select, en passant une variable issu d'ici cela
+                // ne serait pas mieux, à voir
+                'mapped' => false,
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('Ajouter', SubmitType::class)
         ;
     }
 
