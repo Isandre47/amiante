cc69ea6c088b20818f1c2540acf4d316de56e193
Isandre47 <nuanda@live.fr>
2020-05-11 14:35:53 +0200

    alpha add fortyme for initial analyse

--- src/Form/InitialType.php
+++ src/Form/InitialType.php
@@ -2,9 +2,9 @@
 
 namespace App\Form;
 
-use App\Entity\Category;
 use App\Entity\Initial;
 use App\Entity\Zone;
+use App\Repository\ZoneRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -12,13 +12,25 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class InitialType extends AbstractType
 {
+    private $zoneRepository;
+
+    /**
+     * InitialType constructor.
+     * @param $zoneRepository
+     */
+    public function __construct(ZoneRepository $zoneRepository)
+    {
+        $this->zoneRepository = $zoneRepository;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('location')
             ->add('zone', EntityType::class, [
-                'class' => Category::class,
-                'choice_label' => 'name'
+                'class' => Zone::class,
+                'choices' => $this->zoneRepository->findBy(['site' => '1']),
+                'choice_label' => 'category.name'
             ])
         ;
     }
