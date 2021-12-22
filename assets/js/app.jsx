afbd2e2bbfe2274165ccb263e266fac79c9e9329
Isandre47 <nuanda@live.fr>
2021-12-22 16:05:59 +0100

    add react router, alpha version

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from "./Component/Navbar";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import AnotherPage from "./Component/AnotherPage";
+import Dashboard from "./Component/Dashboard";
+import User from "./Component/User";
 
 class App extends Component {
     render() {
       return (
           <div className={'container-fluid'}>
             <div className={'bg-secondary'}>
-              <Navbar />
+              <BrowserRouter>
+                <div className="row">
+                  <Navbar />
+                  <div className={'col-9 shadow bg-light'}>
+                    <br/>
+                    <Routes>
+                      <Route path={'/another-page'} element={<AnotherPage />}/>
+                      <Route path={'/dashboard'} element={<Dashboard />}/>
+                      <Route path={'/users'} element={<User />}/>
+                    </Routes>
+                  </div>
+                </div>
+              </BrowserRouter>
             </div>
           </div>
       )
